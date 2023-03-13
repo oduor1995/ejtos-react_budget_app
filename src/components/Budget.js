0143eb773a28fb4bf4006731271a1b34ff95d949
oduor1995 <otienocollinsoduor@gmail.com>
2023-03-13 15:57:47 -0400

    added new feature

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,71 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+  const { budget, dispatch } = useContext(AppContext);
+  const [editMode, setEditMode] = useState(false);
+  const [newBudget, setNewBudget] = useState(budget);
+
+  const handleIncrease = () => {
+    setNewBudget(prevBudget => {
+      const increasedBudget = prevBudget + 10;
+      if (increasedBudget > 20000) {
+        alert('Budget cannot exceed 20000!');
+        return prevBudget;
+      } else {
+        return increasedBudget;
+      }
+    });
+  };
+
+  const handleDecrease = () => {
+    setNewBudget(prevBudget => prevBudget - 10);
+  };
+
+  const handleSave = () => {
+    if (newBudget > 20000) {
+      alert('Budget cannot exceed 20000!');
+    } else {
+      dispatch({ type: 'SET_BUDGET', payload: newBudget });
+    }
+    setEditMode(false);
+  };
+
+  const handleCancel = () => {
+    setEditMode(false);
+    setNewBudget(budget);
+  };
+
+  return (
+    <div className="alert alert-secondary">
+      {editMode ? (
+        <>
+          <input
+            type="number"
+            value={newBudget}
+            onChange={(e) => setNewBudget(parseInt(e.target.value))}
+            max="20000"
+          />
+          <button onClick={handleSave}>Save</button>
+          <button onClick={handleCancel}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <span>Budget: £{budget}</span>
+          <button onClick={() => setEditMode(true)}>Edit</button>
+          <button onClick={handleIncrease}>Increase</button>
+          <button onClick={handleDecrease}>Decrease</button>
+        </>
+      )}
+    </div>
+  );
 };
+
 export default Budget;
+
+
+
+
+
+
+
